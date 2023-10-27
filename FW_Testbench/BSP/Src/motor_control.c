92d00501cf83635e10f136a2fecb1a857f538f23
Thomas Ruiz <truizw@gmail.com>
2023-10-27 13:54:00 -0400

    Functional direction and speed change

--- FW_Testbench/BSP/Src/motor_control.c
+++ FW_Testbench/BSP/Src/motor_control.c
@@ -15,14 +15,12 @@
 
 /* Private functions declaration */
 static void Modify_Speed(void);
-static void Modify_Direction(uint16_t difference);
+static void Modify_Direction(int16_t difference);
 
 /* Global variables */
 struct Motor_t g_base_motor =
 {
 	.is_motor_initialized = false,
-
-
 };
 
 /* Function implementation */
@@ -37,15 +35,12 @@ void MotorControl_Init(void)
 	g_base_motor.motor_timer_handle = &htim2;
 	g_base_motor.motor_timer_channel = TIM_CHANNEL_1;
 
-	HAL_TIM_PWM_Start(g_base_motor.motor_timer_handle, g_base_motor.motor_timer_channel);
-
 	g_base_motor.motor_timer_handle->Instance->ARR = 2500;
 	g_base_motor.motor_timer_handle->Instance->CCR1 = g_base_motor.motor_timer_handle->Instance->ARR / 2;
 
-
 	g_base_motor.motor_angle_to_reach = 0;
 
-	g_base_motor.deg_per_turn = 0.9f;
+	g_base_motor.deg_per_turn = 0.06f;
 	g_base_motor.current_motor_angle = 0;
 	g_base_motor.nb_pulse = 0;
 	g_base_motor.delay = 0;
@@ -58,36 +53,35 @@ void MotorControl_Init(void)
  */
 void MotorControl_Task(void)
 {
-	uint16_t difference = g_base_motor.motor_angle_to_reach - g_base_motor.current_motor_angle;
+	int16_t difference = g_base_motor.motor_angle_to_reach - g_base_motor.current_motor_angle;
+
 	if (difference != 0)
 	{
 		Modify_Direction(difference);
 		Modify_Speed();
 	}
-
 }
 
 void Modify_Speed(void)
 {
-
-	uint16_t new_freq = g_base_motor.motor_speed_percent * FREQ_MAX_HZ;
+	uint16_t new_freq = (g_base_motor.motor_speed_percent * FREQ_MAX_HZ) / 100;
 	uint16_t new_arr = (FREQ_CLK_HZ / ((PSC + 1)* new_freq)) - 1;
-
 
 	g_base_motor.motor_timer_handle->Instance->ARR = new_arr;
 	g_base_motor.motor_timer_handle->Instance->CCR1 = g_base_motor.motor_timer_handle->Instance->ARR / 2;
 
-	g_base_motor.nb_pulse = (g_base_motor.motor_angle_to_reach - g_base_motor.current_motor_angle) / g_base_motor.deg_per_turn ;
+	g_base_motor.nb_pulse = (abs(g_base_motor.motor_angle_to_reach - g_base_motor.current_motor_angle)) / g_base_motor.deg_per_turn ;
 	g_base_motor.delay = ((float)g_base_motor.nb_pulse / (float)new_freq) * 1000;
 
-	g_base_motor.current_motor_angle = g_base_motor.motor_angle_to_reach;
+	HAL_TIM_PWM_Start(g_base_motor.motor_timer_handle, g_base_motor.motor_timer_channel);
+	HAL_Delay(g_base_motor.delay);
+	HAL_TIM_PWM_Stop(g_base_motor.motor_timer_handle, g_base_motor.motor_timer_channel);
 
+	g_base_motor.current_motor_angle = g_base_motor.motor_angle_to_reach;
 }
 
-void Modify_Direction(uint16_t difference)
+void Modify_Direction(int16_t difference)
 {
-
-
 	if (difference < 0)
 	{
 		g_base_motor.motor_direction = DIRECTION_COUNTERCLOCKWISE;
@@ -106,12 +100,9 @@ void Modify_Direction(uint16_t difference)
 	if (g_base_motor.motor_direction == DIRECTION_CLOCKWISE)
 	{
 		HAL_GPIO_WritePin(DIR_Motor_1_GPIO_Port, DIR_Motor_1_Pin, GPIO_PIN_SET);
-
 	}
     else if (g_base_motor.motor_direction == DIRECTION_COUNTERCLOCKWISE)
 	{
     	HAL_GPIO_WritePin(DIR_Motor_1_GPIO_Port, DIR_Motor_1_Pin, GPIO_PIN_RESET);
 	}
-
-
 }
